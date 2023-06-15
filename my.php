8245c022ba6040dc2cab4fd165aa9148ba85ea38
infotellers <136674066+infotellers@users.noreply.github.com>
2023-06-15 10:31:24 +0500

    Add files via upload

---
+++ my.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Infotellers</title>
+</head>
+<body>
+  <h1>Welcome to Infotellers</h1>
+  <p>Welcome to Infotellers, a website dedicated to delivering the latest news and information on various topics that matter to you. Our goal is to provide accurate and up-to-date information on a wide range of subjects, including technology, science, business, health, entertainment, and more. Explore our website at <a href="https://www.infotellers.com">infotellers.com</a> to stay informed and gain valuable insights into the world around us.</p>
+
+  <?php
+  // Additional PHP code can be added here if needed
+  ?>
+</body>
+</html>
